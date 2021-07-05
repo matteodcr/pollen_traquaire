258ba868df58165cc2fc5113a268ff2f80840b61
Edgar Onghena <46636609+edgarogh@users.noreply.github.com>
2021-07-06 00:31:51 +0200

    Deserialize in one go

--- src/scrapper.rs
+++ src/scrapper.rs
@@ -1,11 +1,22 @@
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Deserializer};
 use std::collections::HashMap;
 
 const URL_MAP: &str = "https://www.pollens.fr/load_vigilance_map";
 
 #[derive(serde::Deserialize, Debug)]
 struct Map {
-    #[serde(rename = "vigilanceMapCounties")]
-    vigilance_map_counties: String,
+    #[serde(rename = "vigilanceMapCounties", deserialize_with = "de_str_as_json")]
+    vigilance_map_counties: HashMap<u8, Departement>,
+}
+
+/// Désérialise une chaîne de caractère en tant que JSON
+fn de_str_as_json<'de, D: Deserializer<'de>, T: DeserializeOwned>(
+    deserializer: D,
+) -> Result<T, D::Error> {
+    use serde::de::Error;
+    let str = String::deserialize(deserializer)?;
+    serde_json::from_str(&str).map_err(D::Error::custom)
 }
 
 #[derive(serde::Deserialize, Debug)]
@@ -25,5 +36,5 @@ pub struct Risk {
 
 pub async fn scrap() -> HashMap<u8, Departement> {
     let reponse: Map = reqwest::get(URL_MAP).await.unwrap().json().await.unwrap();
-    serde_json::from_str(&reponse.vigilance_map_counties).unwrap()
+    reponse.vigilance_map_counties
 }
