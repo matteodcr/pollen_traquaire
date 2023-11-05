f0e08f031fda947c4627b880fca297f0f09d346b
Edgar Onghena <dev@edgar.bzh>
2023-11-04 20:22:08 -0400

    Fix API break

--- src/scrapper.rs
+++ src/scrapper.rs
@@ -1,4 +1,4 @@
-use serde::de::DeserializeOwned;
+use serde::de::{DeserializeOwned, Error, Unexpected};
 use serde::{Deserialize, Deserializer};
 use std::collections::HashMap;
 
@@ -7,16 +7,35 @@ const URL_MAP: &str = "https://www.pollens.fr/load_vigilance_map";
 #[derive(serde::Deserialize, Debug)]
 struct Map {
     #[serde(rename = "vigilanceMapCounties", deserialize_with = "de_str_as_json")]
-    vigilance_map_counties: HashMap<u8, Departement>,
+    vigilance_map_counties: HashMap<DepartementNum, Departement>,
 }
 
 /// Désérialise une chaîne de caractère en tant que JSON
 fn de_str_as_json<'de, D: Deserializer<'de>, T: DeserializeOwned>(
     deserializer: D,
 ) -> Result<T, D::Error> {
-    use serde::de::Error;
     let str = std::borrow::Cow::<str>::deserialize(deserializer)?;
     serde_json::from_str(str.as_ref()).map_err(D::Error::custom)
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[repr(transparent)]
+pub struct DepartementNum(pub u8);
+
+impl<'de> Deserialize<'de> for DepartementNum {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let num_str = <&str>::deserialize(deserializer)?;
+        match num_str.trim_start_matches('0').parse::<u8>() {
+            Ok(num) => Ok(Self(num)),
+            Err(_) => Err(D::Error::invalid_value(
+                Unexpected::Str(num_str),
+                &"a valid french county number",
+            )),
+        }
+    }
 }
 
 #[derive(serde::Deserialize, Debug)]
@@ -34,7 +53,7 @@ pub struct Risk {
     pub level: u8,
 }
 
-pub async fn scrap() -> HashMap<u8, Departement> {
+pub async fn scrap() -> HashMap<DepartementNum, Departement> {
     let reponse: Map = reqwest::get(URL_MAP).await.unwrap().json().await.unwrap();
     reponse.vigilance_map_counties
 }
